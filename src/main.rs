24d918f9b733fbf37d781994ba3eb13a4366fe10
reversed-R <qiu1287shan@gmail.com>
2026-03-17 12:25:34 +0900

    feat: fmt::Write writeln!()

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@
 
 use core::arch::asm;
 use core::cmp::min;
+use core::fmt;
+use core::fmt::Write;
 use core::mem::offset_of;
 use core::panic::PanicInfo;
 use core::ptr::null_mut;
@@ -42,6 +44,11 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         draw_font_fg(&mut vram, i as i64 * 16 + 256, i as i64 * 16, 0xffffff, c);
     }
     draw_str_fg(&mut vram, 256, 256, 0xffffff, "Hello, World!");
+
+    let mut w = VramTextWriter::new(&mut vram);
+    for i in 0..4 {
+        writeln!(w, "i = {i}").unwrap();
+    }
 
     // println!("Hello, world!");
     loop {
@@ -339,6 +346,38 @@ fn lookup_font(c: char) -> Option<[[char; 8]; 16]> {
     }
 
     None
+}
+
+struct VramTextWriter<'a> {
+    vram: &'a mut VramBufferInfo,
+    cursor_x: i64,
+    cursor_y: i64,
+}
+
+impl<'a> VramTextWriter<'a> {
+    fn new(vram: &'a mut VramBufferInfo) -> Self {
+        Self {
+            vram,
+            cursor_x: 0,
+            cursor_y: 0,
+        }
+    }
+}
+
+impl fmt::Write for VramTextWriter<'_> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars() {
+            if c == '\n' {
+                self.cursor_y += 16;
+                self.cursor_x = 0;
+            } else {
+                draw_font_fg(self.vram, self.cursor_x, self.cursor_y, 0xffffff, c);
+                self.cursor_x += 8;
+            }
+        }
+
+        Ok(())
+    }
 }
 
 #[panic_handler]
