aea7fbda929c149c7f61a085578f37683ecbb259
reversed-R <qiu1287shan@gmail.com>
2026-03-18 21:43:27 +0900

    feat: first fit allocator

---
+++ src/allocator.rs
@@ -0,0 +1,224 @@
+use core::{
+    alloc::{GlobalAlloc, Layout},
+    borrow::BorrowMut,
+    cell::RefCell,
+    cmp::max,
+    fmt,
+    ops::DerefMut,
+    ptr::null_mut,
+};
+
+use crate::{
+    result::Result,
+    uefi::{EfiMemoryDescriptor, EfiMemoryType, MemoryMapHolder},
+};
+
+extern crate alloc;
+
+use alloc::boxed::Box;
+
+pub fn round_up_to_nearest_pow2(v: usize) -> Result<usize> {
+    1usize
+        .checked_shl(usize::BITS - v.wrapping_sub(1).leading_zeros())
+        .ok_or("Out of range")
+}
+
+/// `|` は以下の図でHeaderをもつチャンク表す
+/// before: |-- prev --------|---- self ------------------
+/// align:  |--------|--------|--------|--------|--------|
+/// after:  |-------- -------||--------|------------------
+struct Header {
+    next_header: Option<Box<Header>>,
+    size: usize,
+    is_allocated: bool,
+    _reserved: usize,
+}
+
+const HEADER_SIZE: usize = size_of::<Header>();
+const _: () = assert!(HEADER_SIZE == 32);
+// Header のサイズは2のべき乗でなければならない
+const _: () = assert!(HEADER_SIZE.count_ones() == 1);
+
+pub const LAYOUT_PAGE_4K: Layout = unsafe { Layout::from_size_align_unchecked(4096, 4096) };
+
+impl Header {
+    fn can_provide(&self, size: usize, align: usize) -> bool {
+        // NOTE: この検査は大雑把なものである
+        // 実際にはより小さいサイズしか必要ない場合もある
+        // HEADER_SIZE * 2 : 1つはアロケートするための領域、もう1つはパディング
+        self.size >= size + HEADER_SIZE * 2 + align
+    }
+
+    fn is_allocated(&self) -> bool {
+        self.is_allocated
+    }
+
+    fn end_addr(&self) -> usize {
+        self as *const Header as usize + self.size
+    }
+
+    unsafe fn new_from_addr(addr: usize) -> Box<Self> {
+        let header = addr as *mut Self;
+        unsafe {
+            header.write(Self {
+                next_header: None,
+                size: 0,
+                is_allocated: false,
+                _reserved: 0,
+            });
+
+            Box::from_raw(addr as *mut Self)
+        }
+    }
+
+    unsafe fn from_allocated_region(addr: *mut u8) -> Box<Self> {
+        unsafe {
+            let header = addr.sub(HEADER_SIZE) as *mut Self;
+            Box::from_raw(header)
+        }
+    }
+
+    fn provide(&mut self, size: usize, align: usize) -> Option<*mut u8> {
+        let size = max(round_up_to_nearest_pow2(size).ok()?, HEADER_SIZE);
+        let align = max(align, HEADER_SIZE);
+        if self.is_allocated() || !self.can_provide(size, align) {
+            None
+        } else {
+            // 以下各文字は32bytesのチャンクを表す
+            //
+            // |----|----------------- self ----------|--------
+            // |----|----------------------           |--------
+            //                                        ^ self.end_addr()
+            //                              |-------|-
+            //                              ^ header_for_allocated
+            //                               ^ allocated_addr
+            //                                      ^ header_for_padding
+            //                                      ^
+            // header_for_allocated.end_addr() は要求されたオブジェクトを
+            // 割り当てるのに十分な空間を持つ
+
+            let mut size_used = 0;
+            let allocated_addr = (self.end_addr() - size) & !(align - 1);
+            let mut header_for_allocated =
+                unsafe { Self::new_from_addr(allocated_addr - HEADER_SIZE) };
+            header_for_allocated.is_allocated = true;
+            header_for_allocated.size = size + HEADER_SIZE;
+            size_used += header_for_allocated.size;
+            header_for_allocated.next_header = self.next_header.take();
+            if header_for_allocated.end_addr() != self.end_addr() {
+                // パディングのためのHeaderを作る
+                let mut header_for_padding =
+                    unsafe { Self::new_from_addr(header_for_allocated.end_addr()) };
+                header_for_padding.is_allocated = false;
+                header_for_padding.size = self.end_addr() - header_for_allocated.end_addr();
+                size_used += header_for_padding.size;
+                header_for_padding.next_header = header_for_allocated.next_header.take();
+                header_for_allocated.next_header = Some(header_for_padding)
+            }
+
+            // self を縮小
+            assert!(self.size >= size_used + HEADER_SIZE);
+            self.size -= size_used;
+            self.next_header = Some(header_for_allocated);
+
+            Some(allocated_addr as *mut u8)
+        }
+    }
+}
+
+impl Drop for Header {
+    fn drop(&mut self) {
+        panic!("Header should not be dropped!");
+    }
+}
+
+impl fmt::Debug for Header {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Header @ {:#018X} {{ size: {:#018X}, is_allocated: {} }}",
+            self as *const Header as usize,
+            self.size,
+            self.is_allocated()
+        )
+    }
+}
+
+pub struct FirstFitAllocator {
+    first_header: RefCell<Option<Box<Header>>>,
+}
+
+#[global_allocator]
+pub static ALLOCATOR: FirstFitAllocator = FirstFitAllocator {
+    first_header: RefCell::new(None),
+};
+
+// 現時点ではFirstFitAllocatorはスレッドセーフではないが、Syncを満たすためにimpl
+unsafe impl Sync for FirstFitAllocator {}
+
+unsafe impl GlobalAlloc for FirstFitAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        self.alloc_with_options(layout)
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        let mut region = unsafe { Header::from_allocated_region(ptr) };
+        region.is_allocated = false;
+        Box::leak(region);
+    }
+}
+
+impl FirstFitAllocator {
+    pub fn alloc_with_options(&self, layout: Layout) -> *mut u8 {
+        let mut header = self.first_header.borrow_mut();
+        let mut header = header.deref_mut();
+
+        loop {
+            match header {
+                Some(e) => match e.provide(layout.size(), layout.align()) {
+                    Some(p) => break p,
+                    None => {
+                        header = e.next_header.borrow_mut();
+                        continue;
+                    }
+                },
+                None => break null_mut(),
+            }
+        }
+    }
+
+    pub fn init_with_mmap(&self, memory_map: &MemoryMapHolder) {
+        for e in memory_map.iter() {
+            if e.memory_type() == EfiMemoryType::CONVENTIONAL_MEMORY {
+                self.add_free_from_descriptor(e);
+            }
+        }
+    }
+
+    fn add_free_from_descriptor(&self, desc: &EfiMemoryDescriptor) {
+        let mut start_addr = desc.physical_start() as usize;
+        let mut size = desc.number_of_pages() as usize * 4096;
+
+        // allocator が address 0 をフリー領域に含まないことを保証する
+        if start_addr == 0 {
+            start_addr += 4096;
+            size = size.saturating_sub(4096);
+        }
+        if size <= 4096 {
+            return;
+        }
+
+        let mut header = unsafe { Header::new_from_addr(start_addr) };
+        header.next_header = None;
+        header.is_allocated = false;
+        header.size = size;
+        let mut first_header = self.first_header.borrow_mut();
+        let prev_last = first_header.replace(header);
+        drop(first_header);
+        let mut header = self.first_header.borrow_mut();
+        header.as_mut().unwrap().next_header = prev_last;
+        // メモリ上に書かれているすべての領域は隣接しておらず
+        // それらをマージするのは原理的に不可能であるため
+        // ここでHeaderたちがソートされていないのは問題ない
+    }
+}
--- src/uefi.rs
+++ src/uefi.rs
@@ -152,6 +152,10 @@ impl EfiMemoryDescriptor {
         self.memory_type
     }
 
+    pub fn physical_start(&self) -> u64 {
+        self.physical_start
+    }
+
     pub fn number_of_pages(&self) -> u64 {
         self.number_of_pages
     }
